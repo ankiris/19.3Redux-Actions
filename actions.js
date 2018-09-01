a9d813ca2eac19bfcf9a1d5c0f40733276a4e684
Kat Lasota <lasota.kat@gmail.com>
2018-09-01 16:51:12 +0200

    first commit

---
+++ actions.js
@@ -0,0 +1,44 @@
+import uuid from uuid;
+
+const ADD_COMMENT = 'ADD_COMMENT';
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function addComment(text) {
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid()
+    };
+}
+  
+function removeComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id: id
+    };
+}
+  
+function editComment(text, id) {
+    return {
+        type: EDIT_COMMENT,
+        text: text,
+        id: id
+    };
+}
+  
+function tumbUp(id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id: id
+    };
+}
+  
+function tumbDown(id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id: id
+    };
+}
